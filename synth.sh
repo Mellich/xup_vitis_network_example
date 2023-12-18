5378e81940844af758b18ad2368e69cfbb767715
Marius Meyer <mariusme@mail.upb.de>
2023-12-18 13:57:55 +0100

    Synth script for basic test

---
+++ synth.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#SBATCH -p normal
+#SBATCH -q fpgasynthesis
+#SBATCH -t 12:00:00
+#SBATCH --cpus-per-task=16
+#SBATCH --mem=64g
+
+module reset
+module load fpga xilinx/xrt/2.14
+
+export LM_LICENSE_FILE="27000@kiso.uni-paderborn.de"
+
+make all DEVICE=xilinx_u280_gen3x16_xdma_1_202211_1 INTERFACE=3 DESIGN=basic MAX_SOCKETS=60
